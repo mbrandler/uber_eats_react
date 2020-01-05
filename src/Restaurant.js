2a016ef6c31b7ad07b3fa6d72d9ded49bfd5b1d5
mbrandler <marya.brandler@yandex.ru>
2020-01-05 18:36:18 +0300

    сделала карточки с картинками

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,58 +1,36 @@
 import React, { Component } from 'react'
 
-const Card = () => {
-	return (
-		<div className='row'>
-			<div className="col-lg-4 col-sm-6 col-xs-12">
-						<div className="card">
-			   			{/*<img src={props.restaurant.img} className="card-img-top" alt={props.restaurant.alt}/>*/}
-			 	   		<div className="card-body">
-			    	 		<h5 className="card-title">Макдональдс</h5>
-			     			<p className="card-text">₽₽ • Бургеры</p>
-			     			<p className="card-time">Привет</p>
-			    		</div>
-			  		</div>
-					</div>
-					<div className="col-lg-4 col-sm-6 col-xs-12">
-						<div className="card">
-			   			{/*<img src={props.restaurant.img} className="card-img-top" alt={props.restaurant.alt}/>*/}
-			 	   		<div className="card-body">
-			    	 		<h5 className="card-title">Привет</h5>
-			     			<p className="card-text">Привет</p>
-			     			<p className="card-time">Привет</p>
-			    		</div>
-			  		</div>
-					</div>
-					<div className="col-lg-4 col-sm-6 col-xs-12">
-						<div className="card">
-			   			{/*<img src={props.restaurant.img} className="card-img-top" alt={props.restaurant.alt}/>*/}
-			 	   		<div className="card-body">
-			    	 		<h5 className="card-title">Привет</h5>
-			     			<p className="card-text">Привет</p>
-			     			<p className="card-time">Привет</p>
-			    		</div>
-			  		</div>
-					</div>
-					<div className="col-lg-4 col-sm-6 col-xs-12">
-						<div className="card">
-			   			{/*<img src={props.restaurant.img} className="card-img-top" alt={props.restaurant.alt}/>*/}
-			 	   		<div className="card-body">
-			    	 		<h5 className="card-title">Привет</h5>
-			     			<p className="card-text">Привет</p>
-			     			<p className="card-time">Привет</p>
-			    		</div>
-			  		</div>
-					</div>
-				</div>
-				)
+
+const Card = props => {
+
+	const cards = props.restaurantData.map((card, index) => {
+
+		return (
+			<div className="col-lg-4 col-sm-6 col-xs-12" key={index}>
+				<div className="card">
+				
+   			<img src={card.img} className="card-img-top" alt={card.alt}/>
+	 	   		<div className="card-body">
+	    	 		<h5 className="card-title">{card.name}</h5>
+	     			<p className="card-text">{card.text}</p>
+	     			<p className="card-time">{card.time}</p>
+	    		</div>
+	  		</div>
+			</div>
+		)
+	})
+	return <div className='row'>{cards}</div>
 }
+
+
 
 class Restaurant extends Component {
 
 	render () {
 		return (
-			
-					<Card />
+					
+					<Card restaurantData={this.props.restaurantData} />
+					
 			)
 	}
 }
