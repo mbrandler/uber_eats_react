a3ba17d9c1206d1d975ddbcbaaddefb60cca4fa4
mbrandler <marya.brandler@yandex.ru>
2020-01-11 14:31:20 +0300

    перенесла данные в меню

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,21 +4,49 @@ import Name from './Name.js'
 import Snacks from './Snacks.js'
 import { connect } from 'react-redux'
 
+import seld from '../../img/Uber Eats/Restaurant/seld.png'
+import gribi from '../../img/Uber Eats/Restaurant/gribi.png'
+import semga from '../../img/Uber Eats/Restaurant/semga.png'
+import solenia from '../../img/Uber Eats/Restaurant/solenia.png'
+import salo from '../../img/Uber Eats/Restaurant/salo.png'
+import yazik from '../../img/Uber Eats/Restaurant/yazik.png'
+
+import pushkin from '../../img/Uber Eats/Restaurant/pushkin.png'
+
 class Menu extends Component {
 
   render() {
 
-    console.log('menu',  this.props)
+    
+    // {img: pushkin, text: 'Макдоналдс'},
+    // {img: pushkin, text: 'DimSum & Co'},
+    // {img: pushkin, text: 'ДвижОК'},
+    // {img: pushkin, text: 'Ня'},
+    // {img: pushkin, text: 'Cinnabon'},
+    // {img: pushkin, text: 'PIZZELOVE'},
+    // {img: pushkin, text: 'Zю кафе'},
+    // {img: pushkin, text: 'Bar BQ'},
+  
+
+    const menu =  [
+        {img: seld, name: 'Сельдь на бородинском хлебе', price: '240 ₽', alt: 'Селёдка'},
+        {img: gribi, name: 'Грибы маринованные', price: '300 ₽', alt: 'Грибы'},
+        {img: semga, name: 'Малосольная семга', price: '390 ₽', alt: 'Сёмга'},
+        {img: solenia, name: 'Соленья ассорти', price: '320 ₽', alt: 'Соленья'},
+        {img: salo, name: 'Сало домашнее с горчицей', price: '320 ₽', alt: 'Сало'},
+        {img: yazik, name: 'Язык говяжий с хреном', price: '350 ₽', alt: 'Язык говяжий'},
+      ]
+
+    const restaurant = {img: pushkin, text: 'Трактир «Пушкин»', menu}
+
     return (
         <div className='container'>
           <div className={classes.menu}>
-            <Name />
+            <Name nameData={restaurant}/>
             <h3>Закуски</h3>
-            <Snacks />
+            <Snacks snackData={restaurant.menu}/>
           </div>
         </div>
-        
-  
     );
   }
 }
--- src/components/Menu/Snacks.js
+++ src/components/Menu/Snacks.js
@@ -3,27 +3,13 @@ import React from 'react'
 import { connect } from 'react-redux'
 import classes from './Snacks.module.css'
 
-import seld from '../../img/Uber Eats/Restaurant/seld.png'
-import gribi from '../../img/Uber Eats/Restaurant/gribi.png'
-import semga from '../../img/Uber Eats/Restaurant/semga.png'
-import solenia from '../../img/Uber Eats/Restaurant/solenia.png'
-import salo from '../../img/Uber Eats/Restaurant/salo.png'
-import yazik from '../../img/Uber Eats/Restaurant/yazik.png'
+
 
 
 const Snacks = props => {
-	const snack1 = {
-	snackData: [
-		{img: seld, name: 'Сельдь на бородинском хлебе', price: '240 ₽', alt: 'Селёдка'},
-		{img: gribi, name: 'Грибы маринованные', price: '300 ₽', alt: 'Грибы'},
-		{img: semga, name: 'Малосольная семга', price: '390 ₽', alt: 'Сёмга'},
-		{img: solenia, name: 'Соленья ассорти', price: '320 ₽', alt: 'Соленья'},
-		{img: salo, name: 'Сало домашнее с горчицей', price: '320 ₽', alt: 'Сало'},
-		{img: yazik, name: 'Язык говяжий с хреном', price: '350 ₽', alt: 'Язык говяжий'},
-	]
-}
-	console.log(props)
-	const snacks = snack1.snackData.map((snack, index) => {
+
+	console.log('snacks',props)
+	const snacks = props.snackData.map((snack, index) => {
 
 		return (
 			<div className='col-lg-6'>
@@ -41,7 +27,7 @@ const Snacks = props => {
 				    <div className="col-md-4" >
 				      <img src={snack.img} className="card-img" alt={snack.alt} />
 				    </div>
-				    
+
 				  </div>
 				</div>
 			</div>
