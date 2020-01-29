05e0ffef6a8958a5f9d37e2e29f7c6f926db38f4
mbrandler <marya.brandler@yandex.ru>
2020-01-29 18:21:23 +0300

    add faq css file

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import classes from './FAQ.module.css'
 
 const FAQ = () => {
 	return (
-		<div>
-			<a href="">Ссылка на githab</a>
+		<div className={classes.faq}>
+			<div className='container'>
+				<h1>Здравствуйте!</h1>
+				<h6>Меня зовут Мария</h6>
+				<p>Это мой первый учебный проект в области разработки сайтов.</p>
+				<p>Здесь я постаралась применить полученные мной знания по HTML, CSS, Bootstrap, React, Redux и пр.</p>
+
+				<a href="#">Ссылка на githab</a>
+			</div>
 		</div>
 		)
 }
