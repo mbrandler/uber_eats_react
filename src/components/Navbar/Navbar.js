e705d78680cd58b50d1bb0543e709b220c077ffa
Dmitry <Mobeeze@MacBook-Pro-Ivanov.local>
2020-02-11 22:28:59 +0300

    update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,13 +10,13 @@ const Navbar = () => {
 			<div className="container">
 				<div className="row">
 
-					<div className="col-lg-2  col-md-2  col-sm-5 ">
+					<div className="col-lg-2  col-md-2  col-sm-1">
 						<div className={classes.logo}>
 							<Link to="/"><img src={logo} alt='logo'></img></Link>
 						</div>
 					</div>
 
-					<div className="col-lg-2 offset-lg-8 col-md-3 offset-md-7 col-sm-2">
+					<div className="col-lg-2 offset-lg-8 col-md-3 offset-md-2 col-sm-2">
 						<div className='row'>
 								
 								<div className='col-6 col-sm-0'>	
