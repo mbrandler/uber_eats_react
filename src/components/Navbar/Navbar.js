331bd9102018ed5231ae6273f3589a49971ba6bc
mbrandler <marya.brandler@yandex.ru>
2020-02-15 15:53:00 +0300

    поправила шапку

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,25 +9,13 @@ const Navbar = () => {
 		<div className={classes.header}>
 			<div className="container">
 				<div className="row">
-
-					<div className="col-lg-2  col-md-4  col-sm-4 col-xs-4">
-						<div className={classes.logo}>
-							<Link to="/"><img src={logo} alt='logo'></img></Link>
-						</div>
+					<div className={classes.logo}>
+						<Link to="/"><img src={logo} alt='logo'></img></Link>
 					</div>
 
-					<div className="col-lg-2 offset-lg-8 col-md-4 offset-md-5 col-sm-4 offset-sm-4 col-xs-4 offset-xs-4">
-						{/*<div className='row'>*/}
-								
-								{/*<div className='col-md-2 col-sm-0'>	*/}
-									<button className={classes.sign_in}>Войти</button>
-								{/*</div>*/}
-								
-								{/*<div className='col-md-2'>	*/}
-									<Link to="/basket"><img title="Корзина" className={classes.basket} src={basket} alt='basket'/></Link>
-								{/*</div>*/}
-							  
-						{/*</div>*/}
+					<div className={classes.navbar_items}>		
+						<button className={classes.sign_in}>Войти</button>
+						<Link to="/basket"><img title="Корзина" className={classes.basket} src={basket} alt='basket'/></Link>	  
 					</div>
 
 				</div>
